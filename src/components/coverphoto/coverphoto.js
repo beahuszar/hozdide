b531f602d829cad202d62c998db880061e92706c
Huszar Beata <beata.huszar@adevinta.com>
2020-01-03 22:41:31 +0100

    Make the page a little more professional

---
+++ src/components/coverphoto/coverphoto.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './coverphoto.module.scss';
+
+export default () => {
+  return (
+    <div className={styles.coverPhoto}>
+      <h1>Hozd Ide! Állatorvosi Rendelő</h1>
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Header from '../components/header/header';
+import CoverPhoto from '../components/coverphoto/coverphoto';
 
 export default () => (
   <div>
     <Header />
+    <CoverPhoto />
   </div>
 );
