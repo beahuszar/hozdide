115c5ca2745f1e41936d2251a1471c597320bf9f
Huszar Beata <beata.huszar@adevinta.com>
2020-01-05 17:14:10 +0100

    Create draft Accordion component

--- src/components/toggle/toggle.js
+++ src/components/toggle/toggle.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
-export default props => {
+const listDefinitions = definitions => {
+  if (typeof definitions !== 'undefined') {
+    return definitions.map(definition => (
+      <div key={definition.title}>
+        <dt>{definition.title}</dt>
+        <dd>{definition.description}</dd>
+      </div>
+    ));
+  }
+  return <div>nincs meg semmi sm</div>;
+};
+
+const Toggle = props => {
+  const definitions = listDefinitions(props.definitions);
+
   return (
     <section>
       <h2>{props.title}</h2>
-      <dl>
-        <dt>elso valami</dt>
-        <dd>elso valami lorem ipsuma</dd>
-        <dt>masodik valami</dt>
-        <dd>masodik valami lorem ipsuma</dd>
-        <dt>harmadik valami</dt>
-        <dd>harmadik valami lorem ipsuma</dd>
-      </dl>
+      <dl>{definitions}</dl>
     </section>
   );
 };
+
+export default Toggle;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,31 @@ import Header from '../components/header/header';
 import CoverPhoto from '../components/coverphoto/coverphoto';
 import Toggle from '../components/toggle/toggle';
 
+const descriptionList = [
+  {
+    title: 'Winston',
+    description: 'Fekete macska aoisjifj sdijodfijsdoi sdoifjdfoidsjf',
+  },
+  {
+    title: 'Penny',
+    description: 'Cirmos macska dsiufhisduhfisud sdiufhsidufh sdiufhsidufh',
+  },
+  {
+    title: 'Kandz',
+    description:
+      'sdoifdoijf sdoijfsodifjds sdoijfdsoifj sdoifjdosifj sdofijdoif',
+  },
+  {
+    title: 'Kitty',
+    description:
+      'sdifiuhfiaosufh asdliufhadsiofuh asdiufhadsiofuh asdiufhdisfu',
+  },
+];
+
 export default () => (
   <div>
     <Header />
     <CoverPhoto />
-    <Toggle title="Faszom" />
+    <Toggle title="A kis listam" definitions={descriptionList} />
   </div>
 );
