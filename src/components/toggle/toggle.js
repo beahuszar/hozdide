ee74f904d46f09c5398d6bca8b868c8d44084f56
Huszar Beata <beata.huszar@adevinta.com>
2020-01-05 14:31:31 +0100

    Initial files and structure

---
+++ src/components/toggle/toggle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default props => {
+  return (
+    <section>
+      <h2>{props.title}</h2>
+      <dl>
+        <dt>elso valami</dt>
+        <dd>elso valami lorem ipsuma</dd>
+        <dt>masodik valami</dt>
+        <dd>masodik valami lorem ipsuma</dd>
+        <dt>harmadik valami</dt>
+        <dd>harmadik valami lorem ipsuma</dd>
+      </dl>
+    </section>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/header/header';
 import CoverPhoto from '../components/coverphoto/coverphoto';
+import Toggle from '../components/toggle/toggle';
 
 export default () => (
   <div>
     <Header />
     <CoverPhoto />
+    <Toggle title="Faszom" />
   </div>
 );
