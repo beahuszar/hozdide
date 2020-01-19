7772d6204ed9e222817dd587a427cd548c9e199b
Huszar Beata <beata.huszar@adevinta.com>
2020-01-19 18:28:23 +0100

    Add google maps to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Navbar from '../components/navbar/navbar';
+import Map from '../components/contact/map';
 
 export default ({ data }) => {
   return (
     <div>
       <Navbar />
+      <div>
+        <article style={{ lineHeight: 'normal' }}>
+          Szeretettel várja rendelőjükbe a kedves Gazdikat és kiskedvenceiket
+          Dr. Szabó Orsolya és Dr. Fagyas Zsolt. Pácienseik között fellelhetők
+          kutyák, macskák, nyulak és kisrágcsálók is.
+        </article>
+        <article style={{ lineHeight: 'normal' }}>
+          Szolgáltatásaink: Belgyógyászat Fogászati kezelések Lágy- és
+          csontsebészet Ortopédiai szakrendelés Bőrgyógyászat Szülészet
+          Ivartalanítás Védőoltások beadása Külső és belső paraziták elleni
+          kezelés és védekezés Laborvizsgálat Mikrochip beültetés Állatútlevél
+          kiállítás Digitális röntgen Ultrahang
+        </article>
+      </div>
+      <Map />
     </div>
   );
 };
