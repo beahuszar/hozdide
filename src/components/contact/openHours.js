791684b0b34b54147b18bc53237e3c1265b66e3d
Huszar Beata <beata.huszar@adevinta.com>
2020-01-30 20:10:23 +0100

    Create open hours table

--- src/components/contact/openHours.js
+++ src/components/contact/openHours.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const OpenHours = () => {
-  return <div>Nyitvatartas</div>;
+  const openHrs = useStaticQuery(graphql`
+    query openHrs {
+      site {
+        siteMetadata {
+          openHours {
+            day
+            morning
+            afternoon
+          }
+        }
+      }
+    }
+  `).site.siteMetadata.openHours;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Delelott</th>
+          <th>Delutan</th>
+        </tr>
+      </thead>
+      <tbody>
+        {openHrs.map(row => (
+          <tr>
+            <th>{row.day}</th>
+            <td>{row.morning}</td>
+            <td>{row.afternoon}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default OpenHours;
