2d70f63d5259973249109d1b1af37fdfa185f1b6
Huszar Beata <beata.huszar@adevinta.com>
2020-03-16 17:39:15 +0100

    Add real footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './footer.module.scss';
+
+const Footer = () => {
+  return <div className={s.footerSection}>Impresszum</div>;
+};
+
+export default Footer;
